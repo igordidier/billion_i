388ba0494bf91767931a631040712ffaa6ee087c
igordidier <46652986+igordidier@users.noreply.github.com>
2020-02-05 22:22:29 +0100

    small change

--- includes/handlers/ajax_load_posts.php
+++ includes/handlers/ajax_load_posts.php
@@ -1,10 +1,10 @@
-<?php
-include_once("../../config/config.php");
-include_once("../classes/User.php");
-include_once("../classes/Post.php");
+<?php  
+include("../../config/config.php");
+include("../classes/User.php");
+include("../classes/Post.php");
 
 $limit = 10; //Number of posts to be loaded per call
 
-$posts = new Post($con, $_user['userLoggedIn']); 
-$posts->loadPostsFriends($_user, $limit);
+$posts = new Post($con, $_REQUEST['userLoggedIn']);
+$posts->loadPostsFriends($_REQUEST, $limit);
 ?>
