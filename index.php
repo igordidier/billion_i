2ff9e17af0e968b81f7c085b22e171eb38835717
igordidier <46652986+igordidier@users.noreply.github.com>
2020-02-06 11:54:27 +0100

    b

--- index.php
+++ index.php
@@ -38,22 +38,7 @@ if (isset($_POST['post'])) {
   ?>
 
 
-  <!-- <div class="user_details column">
-		<a href="<?php echo $userLoggedIn; ?>">  <img src="<?php echo $user['profile_pic']; ?>"> </a>
 
-		<div class="user_details_left_right">
-			<a href="<?php echo $userLoggedIn; ?>">
-			<?php
-			echo $user['first_name'] . " " . $user['last_name'];
-
-			 ?>
-			</a>
-			<br>
-			<?php echo "Posts: " . $user['num_posts']. "<br>";
-			echo "Likes: " . $user['num_likes'];
-
-			?>
-		</div> -->
 
 <div class="main_collum">
 
@@ -67,7 +52,11 @@ if (isset($_POST['post'])) {
   </form>
 
   <div class="posts_area"></div>
-  <img id="loading" src="assets/img/icons/loading.gif">
+  
+  <img id="loading" src="assets/img/icons/loading.gif" style="
+    display: flex;
+    margin: auto;
+">
 
 
 </div>
