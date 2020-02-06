0b3de42e2cc64e2d4bcb6371ab95f55c6fdf38f6
igordidier <46652986+igordidier@users.noreply.github.com>
2020-02-06 13:52:13 +0100

    new index still not working

--- index.php
+++ index.php
@@ -52,7 +52,7 @@ if (isset($_POST['post'])) {
   </form>
 
   <div class="posts_area"></div>
-  
+
   <img id="loading" src="assets/img/icons/loading.gif" style="
     display: flex;
     margin: auto;
@@ -62,56 +62,63 @@ if (isset($_POST['post'])) {
 </div>
 
 <script>
-var userLoggedIn = '<?php echo $userLoggedIn; ?>';
+$(function(){
 
-$(document).ready(function() {
+	var userLoggedIn = '<?php echo $userLoggedIn; ?>';
+	var inProgress = false;
 
-  $('#loading').show();
+	loadPosts(); //Load first posts
 
-  //Original ajax request for loading first posts
-  $.ajax({
-    url: "includes/handlers/ajax_load_posts.php",
-    type: "POST",
-    data: "page=1&userLoggedIn=" + userLoggedIn,
-    cache:false,
+    $(window).scroll(function() {
+    	var bottomElement = $(".status_post").last();
+    	var noMorePosts = $('.posts_area').find('.noMorePosts').val();
 
-    success: function(data) {
-      $('#loading').hide();
-      $('.posts_area').html(data);
-    }
-  });
-
-  $(window).scroll(function() {
-    var height = $('.posts_area').height(); //Div containing posts
-    var scroll_top = $(this).scrollTop();
-    var page = $('.posts_area').find('.nextPage').val();
-    var noMorePosts = $('.posts_area').find('.noMorePosts').val();
-
-    if ((document.body.scrollHeight == document.body.scrollTop + window.innerHeight) && noMorePosts == 'false') {
-      $('#loading').show();
-
-      var ajaxReq = $.ajax({
-        url: "includes/handlers/ajax_load_posts.php",
-        type: "POST",
-        data: "page=" + page + "&userLoggedIn=" + userLoggedIn,
-        cache:false,
-
-        success: function(response) {
-          $('.posts_area').find('.nextPage').remove(); //Removes current .nextpage
-          $('.posts_area').find('.noMorePosts').remove(); //Removes current .nextpage
-
-          $('#loading').hide();
-          $('.posts_area').append(response);
+        // isElementInViewport uses getBoundingClientRect(), which requires the HTML DOM object, not the jQuery object. The jQuery equivalent is using [0] as shown below.
+        if (isElementInView(bottomElement[0]) && noMorePosts == 'false') {
+            loadPosts();
         }
-      });
+    });
 
-    } //End if
+    function loadPosts() {
+        if(inProgress) { //If it is already in the process of loading some posts, just return
+			return;
+		}
 
-    return false;
+		inProgress = true;
+		$('#loading').show();
 
-  }); //End (window).scroll(function())
+		var page = $('.posts_area').find('.nextPage').val() || 1; //If .nextPage couldn't be found, it must not be on the page yet (it must be the first time loading posts), so use the value '1'
 
+		$.ajax({
+			url: "includes/handlers/ajax_load_posts.php",
+			type: "POST",
+			data: "page=" + page + "&userLoggedIn=" + userLoggedIn,
+			cache:false,
 
+			success: function(response) {
+				$('.posts_area').find('.nextPage').remove(); //Removes current .nextpage
+				$('.posts_area').find('.noMorePosts').remove(); //Removes current .nextpage
+				$('.posts_area').find('.noMorePostsText').remove(); //Removes current .nextpage
+
+				$('#loading').hide();
+				$(".posts_area").append(response);
+
+				inProgress = false;
+			}
+		});
+    }
+
+    //Check if the element is in view
+    function isElementInView (el) {
+        var rect = el.getBoundingClientRect();
+
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && //* or $(window).height()
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth) //* or $(window).width()
+        );
+    }
 });
 
 </script>
