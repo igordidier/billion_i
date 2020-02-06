e61fe911811c8739463232e9f718af0aae492971
igordidier <46652986+igordidier@users.noreply.github.com>
2020-02-06 23:01:11 +0100

    post work not load

--- includes/classes/post.php
+++ includes/classes/post.php
@@ -27,15 +27,13 @@ class Post {
 			}
 
 			//insert post
-			$query = mysqli_query($this->con, "INSERT INTO posts VALUES('', '$body', '$added_by', '$user_to', '$date_added', 'no', 'no', '0')");
-			$returned_id = mysqli_insert_id($this->con);
+      $query = mysqli_query($this->con, "INSERT INTO posts VALUES(NULL, '$body', '$added_by', '$user_to', '$date_added', 'no', 'no', '0')");
+      $returned_id = mysqli_insert_id($this->con);
 
-			//Insert notification
-
-			//Update post count for user
-			$num_posts = $this->user_obj->getNumPosts();
-			$num_posts++;
-			$update_query = mysqli_query($this->con, "UPDATE users SET num_posts='$num_posts' WHERE username='$added_by'");
+      //Update post count for user
+    			$num_posts = $this->user_obj->getNumPosts(); // get original num_posts
+    			$num_posts++; // add 1
+    			$update_query = mysqli_query($this->con, "UPDATE users SET num_posts='$num_posts' WHERE username='$added_by'");
 
 		}
 	}
