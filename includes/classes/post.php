892ee653473df4e2ab34b09b96881452bcc0c769
igordidier <46652986+igordidier@users.noreply.github.com>
2020-03-03 09:23:46 +0100

    postwork

--- includes/classes/post.php
+++ includes/classes/post.php
@@ -27,7 +27,7 @@ class Post {
 			}
 
 			//insert post
-      $query = mysqli_query($this->con, "INSERT INTO posts VALUES(NULL, '$body', '$added_by', '$user_to', '$date_added', 'no', 'no', '0')");
+      $query = mysqli_query($this->con, "INSERT INTO posts VALUES(NULL, '$body', '$added_by', '$user_to', '$date_added', 'no', 'no', '0',',')");
       $returned_id = mysqli_insert_id($this->con);
 
       //Update post count for user
